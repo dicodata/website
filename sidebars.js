726dd07d2d98c60834b476eb91bc902d3f2977cd
lilstipher <sirmichel1995@gmail.com>
2020-12-06 22:28:11 +0100

    sidebar fix

--- sidebars.js
+++ sidebars.js
@@ -1,10 +1,11 @@
 module.exports = {
   someSidebar: {
-    A : ['A/AB_testing'],
-    B : ['B/backpropagation'],
-    G : ['G/generalization', 'G/generalized_linear_model', 'G/gradient', 'G/gradient_clipping', 'G/gradient_descent', 'G/graph'],
-    U : ['U/unlabeled_example', 'U/unsupervised_machine_learning'],
-    V : ['V/validation_set'],
-    W : ['W/weight', 'W/wide_model'],
+    Docusaurus: ['intro'],
+    A: ['A/AB_testing'],
+    B: ['B/backpropagation'],
+    G: ['G/generalization', 'G/generalized_linear_model', 'G/gradient', 'G/gradient_clipping', 'G/gradient_descent', 'G/graph'],
+    U: ['U/unlabeled_example', 'U/unsupervised_machine_learning'],
+    V: ['V/validation_set'],
+    W: ['W/weight', 'W/wide_model'],
   },
 };
